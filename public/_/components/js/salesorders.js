4e3a5311bb5b86395c2aa1fdd6e117c4a176147f
Jaymar <algoisticdevteam@gmail.com>
2014-12-15 01:30:23 -0800

    Fixes on SOLists

--- public/_/components/js/salesorders.js
+++ public/_/components/js/salesorders.js
@@ -2,6 +2,7 @@
 var itemno = 1;
 var counter = 1;
 function addSO(id){
+	// alert('check');
 	var prodNum = $('#prodId'+id).text();
 	var name= $('#name'+id).text();
 	var brand= $('#brand'+id).text();
@@ -178,7 +179,8 @@ function editSO(id){
 	 	$('#edSOId').text(data[0]['id']);
 	 	$('#edSODate').text(data[0]['SalesOrderDate']);
 	 	$('[name="vwCustomer"]').val(data[0]['CustomerNo']);
-	 	$('[name="medReps"]').val(data[0]['UserNo']);
+	 	// $('[id="medReps"]').prop('selected',true);
+	 	$('select#medReps option[value="'+data[0]['UserNo']+'"]').attr('selected',true);
 	 	if(data[0]['Terms'] == '0'){
 	 		$('#edTerm').val(0);
 	 		$('#edTerm1').addClass('active');
@@ -211,7 +213,6 @@ function editSO(id){
 	  				$('.edSOTable >tbody').append('<tr id="vwPO'+counter+'"><td id="vwItemno'+counter+'">'+counter+'</td><td id="vwProd'+value.ProductNo+'">'+value.ProductNo+'</td>
 	  					<td>'+value.ProductName+'</td>
 	  					<td>'+value.BrandName+'</td>
-	  					<td>'+value.Barcode+'</td>
 	  					<td>'+value.LotNo+'</td>
 						<td>'+value.ExpiryDate+'</td>
 	  					<td><select class="form-control square" name="unit" id="unit'+counter+'">
@@ -300,27 +301,138 @@ function editSO(id){
 				}
 	      });
 }
-function vwaddPO(id){
-	var name= $('#vwname'+id).text();
-	var brand= $('#vwbrand'+id).text();
-	var unit= $('#vwunit'+id).text();
-	var table = $('.vwproduct').DataTable();
-	var index=table.row('#vwrowProd'+id).index();
-	if($('#vwProd'+id).length){prodError
-		 $('#prodError').fadeIn("fast", function(){        
-	        $("#prodError").fadeOut(4000);
-	    });
-	}else{
-	$('.edPOTable >tbody').append('<tr id="vwPO'+counter+'"><td id="vwItemno'+counter+'">'+counter+'</td><td id="vwProd'+id+'">'+id+'</td>
-	  					<td>'+name+'</td>
-	  					<td>'+brand+'</td><td>'+unit+'</td><td class="vweditable" id="edQty'+id+'">'+1+'</td><td class="vweditable" id="edUnit'+id+'">'+1+'</td>
-	  					<td class="ecost"id="edCost'+id+'">1</td><td><button class="btn btn-danger 
-	  					btn-xs square dis" id="vwRemovePO'+counter+'" onclick="vwRemovePO('+counter+')" >
-						<i class="fa fa-times" ></i> Remove</button></td></tr>');
-	
-		counter +=1;
-		editable(id);
+function vwaddSO(id){
+	var prodNum = $('#prodId'+id).text();
+	var name= $('#name'+id).text();
+	var brand= $('#brand'+id).text();
+	var unit= $('#unit'+id).text();
+	var table = $('.product').DataTable();
+	var LotNo = $('#lotNo'+id).text();
+	var ExpiryDate = $('#ExpDate'+id).val();
+	var unitQty = $('#unitQty'+id).text();
+	var unitQtyR = $('#unitQtyR'+id).val();
+	var unitPrice = $('#unitPrice'+id).text();
+	var index=table.row('#rowProd'+id).index();
+	var curDate = new Date();
+	$('.edSOTable').append('<tr id="SO'+itemno+'"><td id="itemno'+itemno+'">'+itemno+'</td>
+		<td id="prdoNo'+itemno+'">'+prodNum+'</td>
+		<td>'+name+'</td>
+		<td>'+brand+'</td>
+		<td>'+LotNo+'</td>
+		<td>'+ExpiryDate+'</td>
+		<td><select class="form-control square" name="unit" id="unit'+itemno+'">
+                  <option value="'+unit+'">'+unit+'</option>
+                  <option value="pcs">pcs</option>
+                </select></td>
+        <input id="unitAv'+id+'" type="hidden" value="'+unitQty+'">
+        <input id="unitAvR'+id+'" type="hidden" value="'+unitQtyR+'">
+		<td class="light-green editable" id="prodQtySO'+itemno+'" value="'+itemno+'">'+1+'</td>
+		<td class="light-red ed" id="prodUntSO'+itemno+'" value="'+unitPrice+'">'+unitPrice+'</td>
+		<td class="ecost" id="prodCostSO'+itemno+'">0.00</td>
+		<td><button class="btn btn-danger btn-xs square" id="removeSO'+itemno+'" onclick="removeSO('+itemno+','+id+','+index+')">
+		<i class="fa fa-times"></i> Remove</button></td></tr>');
+		var unitAvailable = $('#unitAv'+id).val();
+		$('select[id=unit'+itemno+']').on('change', function() {
+			// alert($('select[id=unit'+id+']').val());
+			if($(this).val() == 'pcs'){
+				unitAvailable = $('#unitAvR'+id).val();
+				$('#prodQtySO'+itemno).text('1');
+				// alert(id);
+				// $('#unitQty'+id).hide();
+			}else{
+				// alert('sulod');
+				unitAvailable = $('#unitAv'+id).val();
+				$('#prodQtySO'+itemno).text('1');
+				$('#unitQty'+id).text(unitQty);
+			}
+		});
+		itemno +=1;
+		table.cell( index, 8 ).data('<b class="success"> <i class="fa fa-check-circle"> Added</b>').draw();
+		$('.editable').editable({
+				send: 'never', 
+			    type: 'text',
+			    value:1,
+			    validate: function(value) {
+			        if($.trim(value) == '') {
+			         return 'This field is required';
+			        }
+			        if ($.isNumeric(value) == '' || value==0) {
+			            return 'Please input a valid number greater than 0';
+			        }
+			    },
+			    emptytext:0,
+			   display: function(value) {
+		   		if (parseFloat(value) <= parseFloat(unitAvailable)){
+		   			$('#invalidQty').hide();
+		   			$(this).text(value);
+			   		var rowID = $(this).attr('id').substring(9);
+			   		// alert(rowID);
+			         edcalcCostSO(rowID);
+				}
+				else{
+					// alert('test');
+					$(this).text('1');
+		   			$('#invalidQty').show();
+		   		}
+			  }	
+			});
+		$('.ed').editable({
+				send: 'never', 
+			    type: 'text',
+			    value:unitPrice,
+			    validate: function(value) {
+			        if($.trim(value) == '') {
+			         return 'This field is required';
+			        }
+			        if ($.isNumeric(value) == '' || value==0) {
+			            return 'Please input a valid number greater than 0';
+			        }
+			    },
+			    emptytext:0,
+			   display: function(value) {
+			   		$(this).text(value);
+			   		var rowID = $(this).attr('id').substring(9);
+			   		// alert(rowID);
+			         edcalcCostSO(rowID);
+					}
+				});
+		// $('#edSOTotalCost').text(total);
+				function edcalcCost(id){
+					// alert(id);
+					var qty = $('#edQty'+id).text();
+					var unit = $('#edUnt'+id).text();
+					// alert(unit);
+					$('#edCost'+id).text(parseFloat(qty*unit));
+					edtotalCost();
+				}
+				function edtotalCost(){
+					var total=0;
+					$('.ecost').each(function(){
+						total += parseFloat($(this).text()); 
+					});
+					$('#edSOTotalCost').text(money(total));
+					if(total == 0){
+						$('#vwSaveSOBtn').addClass('hidden');
+					}else{
+						$('#vwSaveSOBtn').removeClass('hidden');
+					}
+				}
 		$('#vwSaveSOBtn').removeClass('hidden');
+}
+function edcalcCostSO(id){
+	var qty = parseInt($('#prodQtySO'+id).text());
+	var unit = parseFloat($('#prodUntSO'+id).text()).toFixed(2);
+	$('#prodCostSO'+id).text(parseFloat(qty*unit).toFixed(2));
+	edtotalCostSO();
+}
+function edtotalCostSO(){
+	var total=0;
+	$('.ecost').each(function(){
+		total += parseFloat($(this).text()); 
+	});
+	$('#edSOTotalCost').text(total.toFixed(2));
+	if(total = 0){
+		$('#vwSaveSOBtn').addClass('hidden');
 	}
 }
 function vwRemovePO(id){
@@ -418,11 +530,12 @@ $(document).ready(function() {
 		var TableData;
 		var id = $('#edSOId').text();
 		var customer = $('[name="vwCustomer"]').val();
+		var UserNo = $('#medReps').val();
 		var term = $('#edTerm').val();
 		TableData = storeTblValues()
 		TableData = $.toJSON(TableData);
 
-		$.post('/saveEditedSO',{TD:TableData,customer:customer,term:term,id:id},function(data){
+		$.post('/saveEditedSO',{TD:TableData,customer:customer,UserNo:UserNo,term:term,id:id},function(data){
 				if(data==1){
 					location.reload();
 				}else if(data==0){
@@ -432,16 +545,17 @@ $(document).ready(function() {
 		function storeTblValues()
 		{
 			var TableData = new Array();
+			var ctr = 1;
 			$('.edSOTable tr').each(function(row, tr){
 			    TableData[row]={
 			        "ProdNo" : $(tr).find('td:eq(1)').text()
-			        , "Barcode" :$(tr).find('td:eq(4)').text()
-			        , "LotNo" :$(tr).find('td:eq(5)').text()
-			        , "ExpiryDate" : $(tr).find('td:eq(6)').text()
-			        , "Unit" :$(tr).find('#unit').val()
-			        , "Qty" : $(tr).find('td:eq(8)').text()
-			        , "CostPerQty" : $(tr).find('td:eq(9)').text()
+			        , "LotNo" : $(tr).find('td:eq(4)').text()
+			        , "ExpiryDate" : $(tr).find('td:eq(5)').text()
+			        , "Unit" : $(tr).find('td:eq(6) select option:selected').val()
+			        , "Qty" : $(tr).find('td:eq(7)').text()
+			        , "UnitPrice" : $(tr).find('td:eq(8)').text()
 			    }
+			    ctr++;
 			});
 			TableData.shift();
 		    return TableData;
